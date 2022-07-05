6730717a5c1cae192ec2cb63feb3c4f98d4bd0ab
JeandersonDev <jeanderson.2315@gmail.com>
2022-07-05 12:52:24 -0300

    Alteração no teste

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -6,5 +6,9 @@ defmodule FizzBuzzTest do
       expected_response = {:ok,[1, 2, :Fizz, 4, :Buzz, :Fizz, 7, 8, :Fizz, :Buzz, :fizzBuzz, :Buzz, :fizzBuzz]}
       assert FizzBuzz.build("numbers.txt") == expected_response
     end
+    test "when an invalid file is inserted, returns a converted list" do
+      expected_response = {:ok,[1, 2, :Fizz, 4, :Buzz, :Fizz, 7, 8, :Fizz, :Buzz, :fizzBuzz, :Buzz, :fizzBuzz]}
+      assert FizzBuzz.build("numbers.txt") == expected_response
+    end
   end
 end
